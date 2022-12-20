822a69be149c2f986a6a2d4a3787373061eae157
Your Name <satya@anils-mbp-2.lan>
2022-12-20 13:49:26 -0600

    Made changes to charts.js

--- Starter_Code/charts.js
+++ Starter_Code/charts.js
@@ -56,22 +56,22 @@ function buildMetadata(sample) {
 
 // Deliverable 1: 1. Create the buildChart function.
 function buildCharts(sample) {
-  // Deliverable 1: 2. Use d3.json to load the samples.json file 
-  d3.json("samples.json").then((data) => {
-    console.log(data);
+  // Deliverable 1: 
+  //2. Use d3.json to load the samples.json file
 
+    d3.json("samples.json").then((data) => {
+    console.log(data);
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-    
     var samplesArray = data.samples;
     console.log(samplesArray);
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-    var selectedIdSamples = samplesArray.filter(data => data.id == samples);
+    var selectedIdSamples = samplesArray.filter(data => data.id == sample);
     console.log(selectedIdSamples);
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     var firstSample = selectedIdSamples[0];
-    console.log(firstsample);
+    console.log(firstSample);
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
