b3f821e6a532e5d008375992c646a34665ca0eeb
Your Name <satya@anils-mbp-2.lan>
2022-12-20 13:27:59 -0600

    commiting deliverable 1 updates

--- Starter_Code/charts.js
+++ Starter_Code/charts.js
@@ -2,7 +2,8 @@ function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
+  /
+  / Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
 
